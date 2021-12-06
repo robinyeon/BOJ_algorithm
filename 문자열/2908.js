f8a1ccf64a4f71be9d126ee42f85f8a975ed0ed6
robinyeon <robin.yeon@gmail.com>
2021-12-07 01:03:22 +0900

    문자열

--- 문자열/2908.js
+++ 문자열/2908.js
@@ -1,2 +1,10 @@
 // 상수
 
+let input = require("fs").readFileSync("/dev/stdin").toString().trim().split(" ");
+
+let a = Number(input[0].split("").reverse().join(""));
+let b = Number(input[1].split("").reverse().join(""));
+
+console.log(a > b ? a : b);
+// console.log(Math.max(a,b));
+
