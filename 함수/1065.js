da574edf825d80be146c9cf8cb21100b7f694f57
robinyeon <robin.yeon@gmail.com>
2021-12-07 23:07:31 +0900

    셀프넘버, 한수

--- 함수/1065.js
+++ 함수/1065.js
@@ -1,2 +1,24 @@
 // 한수
 
+let input = require('fs').readFileSync('/dev/stdin').toString().trim();
+
+let num = Number(input);
+
+function hansu(n) {
+    let count = 0;
+    for (let i = 1; i <= n; i++) {
+        let hundredNumber = Math.floor((i % 1000) / 100);
+        let tenNumber = Math.floor((i % 100) / 10);
+        let oneNumber = i % 10;
+
+        if (i < 100) {
+            count++;
+        } else if (100 <= i && i < 1000) {
+            if (hundredNumber - tenNumber === tenNumber - oneNumber) {
+                count++;
+            }
+        }
+    }
+    return count;
+}
+console.log(hansu(num));
--- 함수/4673.js
+++ 함수/4673.js
@@ -14,10 +14,13 @@ for (let i = 0; i < 10001; i++) {
     trueArr[d(i)] = false;
 }
 
-for (let i = 1; i < 10000; i++) {
+for (let i = 1; i < 10001; i++) {
     if (trueArr[i]) {
         console.log(i);
     }
 }
 
+
+// d(n)함수를 만들때 sum을 따로 만들어 return sum+n을 하지 않고,
+// n+= Number(String(n)[i])으로 return n을 하게 하니 출력초과 됨.
 
